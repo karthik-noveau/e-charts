05658826bd6563771e7e0d17d3789bf0ef28b031
karthik <karthik5112001@gmail.com>
2024-03-20 11:53:06 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,42 +1,15 @@
 import { EChart } from "./components";
+
 import { basicAreaChart } from "./components/options_config/basicAreaChart";
-import { DoughnutChartOptions } from "./components/options_config/doughnutChart";
-import { horizontalBarChartOptions } from "./components/options_config/horizontalBarChart";
-import { lineChartOptions } from "./components/options_config/lineChart";
-import { pieChartOptions } from "./components/options_config/pieChart";
-import { scatterChartOptions } from "./components/options_config/scatterChart";
 import { stackedAreaChartOptions } from "./components/options_config/stackedAreaChart";
-import { stackedVerticalBarChartOptions } from "./components/options_config/stackedVerticalBarChart";
-import { verticalBarChartOptions } from "./components/options_config/verticalBarChart";
 
 import "./components/styles.css";
 
 function App() {
   return (
     <div className="mainContainer">
-      <EChart options={pieChartOptions} />
-      <EChart options={DoughnutChartOptions} />
-      <EChart options={verticalBarChartOptions} />
-      <EChart options={horizontalBarChartOptions} />
-      <EChart options={stackedVerticalBarChartOptions} />
-      <EChart options={lineChartOptions} />
-      <EChart options={scatterChartOptions} />
-      <EChart
-        options={{
-          ...basicAreaChart,
-        }}
-      />
-      <EChart
-        options={{
-          // aria: {
-          //   enabled: true,
-          //   decal: {
-          //     show: true,
-          //   },
-          // },
-          ...stackedAreaChartOptions,
-        }}
-      />
+      <EChart options={basicAreaChart} />
+      <EChart options={stackedAreaChartOptions} />
     </div>
   );
 }
